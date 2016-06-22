ea10c6f2fe81381ccb17e8a68adff5d6907603a1
Paul FLEUTRY <paul.fleutry@telecom-paristech.fr>
2016-06-22 15:06:49 +0200

    Le programme marche bien pour un point [0 0 15]

--- 2016/objecttoWRP.m
+++ 2016/objecttoWRP.m
@@ -5,7 +5,14 @@ lambda = 633*10^-9;
 tailleWRP = 500 ; %valeurs a tester ; WRP de 500x500 pixels pour le moment
 WRP = zeros(tailleWRP, tailleWRP); %WRP carre
 
-a0 = 100; %amplitude de l'onde emise par chaque point, supposee constante
+a0 = 10; %amplitude de l'onde emise par chaque point, supposee constante
+
+sampling = 0.0254/tailleWRP;
+
+dimensions = 1;
+range=dimensions*0.0254/2;
+ipx=(-1*range):sampling:range;
+ipy=(-1*range):sampling:range;
 
 for xWRP = 1:tailleWRP
     fprintf('Calcul pour xWRP = %d sur %d valeurs \n', xWRP, tailleWRP);
@@ -14,7 +21,7 @@ for xWRP = 1:tailleWRP
             xj = M(pointnumber, 1);
             yj = M(pointnumber, 2);
             zj = M(pointnumber, 3);
-            Rwj = sqrt((xWRP-xj)^2+(yWRP-yj)^2+(zj)^2); %distance objet-point du WRP
+            Rwj = sqrt((ipx(xWRP)-xj)^2+(ipy(yWRP)-yj)^2+(zj)^2); %distance objet-point du WRP
             WRP(xWRP, yWRP) = WRP(xWRP, yWRP) + (a0/Rwj)*exp(1i*2*pi*Rwj/lambda); %amplitude complexe sur le WRP
         end
     end
