adfaff9657b8afd7ae966054551f3c2a3903c9e9
Paul FLEUTRY <paul.fleutry@telecom-paristech.fr>
2016-06-24 17:37:46 +0200

    Création d'une barre de progression pour suivre le calcul. Ajout de commentaires.

--- 2016/ob2wrp.m
+++ 2016/ob2wrp.m
@@ -8,28 +8,32 @@ tic;
 
 sampling = Lo/N;
 Nw= floor((N - (Lo-Lw)/sampling)/2)*2; %Taille utile en pixels (N inclut les pixels du 0-padding)
-fprintf('A \n', Nw);
+
 a0 = 10; %amplitude de l'onde emise par chaque point, supposee constante
 WRP = zeros(Nw, Nw); %WRP carre
 
 range=Lw/2;
-ipx=(-1*range):sampling:range;
-ipy=(-1*range):sampling:range;
+ipx=(-1*range):sampling:range; %coordonnees x des pixels en metres, centre sur 0
+ipy=(-1*range):sampling:range; %coordonnees y des pixels en metres, centre sur 0
+
+h=waitbar(0, 'Début du calcul'); %creation d'une barre de progression
 
 for xWRP = 1:Nw
-    fprintf('Calcul pour xWRP = %d sur %d valeurs \n', xWRP, Nw);
+    waitbar(xWRP/Nw,h,sprintf('Calcul pour xWRP = %d sur %d valeurs \n', xWRP, Nw)) %actualisation de la barre de progression
+    %fprintf('Calcul pour xWRP = %d sur %d valeurs \n', xWRP, Nw);
     for yWRP = 1:Nw
         for j=1:size(M,1)
-            xj = M(j, 1);
-            yj = M(j, 2);
-            zj = M(j, 3);
+            xj = M(j, 1); %coordonnee x du point de l'objet
+            yj = M(j, 2); %coordonnee y du point de l'objet
+            zj = M(j, 3); %coordonnee z du point de l'objet
             Rwj = sqrt((ipx(xWRP)-xj)^2+(ipy(yWRP)-yj)^2+(zj+d)^2); %distance objet-point du WRP
             WRP(xWRP, yWRP) = WRP(xWRP, yWRP) + (a0/Rwj)*exp(1i*2*pi*Rwj/lambda); %amplitude complexe sur le WRP
         end
     end
 end
 
-%matriceLCD=[zeros(290,1080);zeros(Nw,290),WRP,zeros(Nw,);zeros(290,1080)];
+delete(h); %fermeture automatique de la barre de progression
+
 WRP = [zeros((N-Nw)/2,Nw);WRP;zeros((N-Nw)/2,Nw)]; %ajout d'un padding pour atteindre 1080 x 1080
 WRP = [zeros(N,(N-Nw)/2),WRP,zeros(N,(N-Nw)/2)];
 
